9446a3540fb27f835eb4396410ef33518e4ab432
michaelryu <michaelryu0@gmail.com>
2014-10-16 15:04:22 -0700

    Finish user edit, update, index, and destroy actions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,23 +4,17 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Brian Ho Hate Club"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Brian Ho Hate Club"
   end
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Brian Ho Hate Club"
   end
 end
