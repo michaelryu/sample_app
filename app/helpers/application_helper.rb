18fc8669f42bdf1bee397356ecf116156fbc65b1
michaelryu <michaelryu0@gmail.com>
2014-11-02 13:59:50 -0800

    removed inside joke

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = "Brian Ho Hate Club"
+    base_title = "Twitter Clone"
     if page_title.empty?
       base_title
     else
